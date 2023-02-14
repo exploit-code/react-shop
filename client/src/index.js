fe20006a0d48ca0024de2ff97eef9dc02a021039
ychkydyk <zavdv1@gmail.com>
2023-02-14 05:48:41 +0300

    подгрузка товаров с бэка.(главная,списки,карточка)

--- client/src/index.js
+++ client/src/index.js
@@ -5,11 +5,20 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import UserContext from './context/UserContext';
 
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "./redux/store";
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <UserContext>
+            <Provider store={store}>
+                <PersistGate loading={"loading"} persistor={persistor}>
             <App />
+                </PersistGate>
+            </Provider>
         </UserContext>
     </React.StrictMode>
 );
