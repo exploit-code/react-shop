1e04db2896579235edc08e6f93dc0a6b67e30895
aborushko <o_xx@list.ru>
2023-02-15 20:25:53 +0300

    up header

--- client/src/Layout/Layout.jsx
+++ client/src/Layout/Layout.jsx
@@ -2,14 +2,11 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
-import UpMainPage from '../components/UpMainPage/UpMainPage'
-
 
 const Layout = () => {
     return (
         <>
             <Header></Header>
-            {/* <UpMainPage /> */}
             <main className='main container'>
                 <Outlet></Outlet>
             </main>
