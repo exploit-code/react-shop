144574eb9909002bb09547ee7bc0a603cfb3ad14
Max1k86 <hasanovmaxim@yandex.ru>
2023-02-17 22:17:46 +0500

    Корзина completed

--- client/src/components/Basket/Basket.jsx
+++ client/src/components/Basket/Basket.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
-import { AuthContext} from "../../context/UserContext";
+import { Link } from 'react-router-dom';
+import { AuthContext } from "../../context/UserContext";
 import './basket-style.scss';
 import Raspberry from './img/raspberry.png'
 import BasketPng from './img/basket.png'
@@ -8,7 +9,7 @@ const Basket = () => {
     const { cartItems } = useContext(AuthContext)
     return (
         <>
-            <main className="page-basket-wrp">
+            <div className="page-basket-wrp">
                 <div className="basket">
                     <header className='head-basket-wrp'>
                         <h3 className='head-basket-title'>
@@ -18,87 +19,44 @@ const Basket = () => {
                             <p className='head-basket-txt_2'>Total Price: $<span>333.00</span></p>
                         </div>
                     </header>
-                    <section className='basket-products'>
-                        <div className="basket-items-wrp">
-                            <img src={Raspberry} alt="img" />
-                            <div className='basket-item'>
-                                <h4 className='basket-item-title'>
-                                    Fresh Raspberry
-                                </h4>
-                                <p className='basket-item-txt'>$30.00</p>
-                            </div>
-                        </div>
-                        <div className="basket-items-wrp">
-                            <img src={Raspberry} alt="img" />
-                            <div className='basket-item'>
-                                <h4 className='basket-item-title'>
-                                    Fresh Raspberry
-                                </h4>
-                                <p className='basket-item-txt'>$30.00</p>
-                            </div>
-                        </div>
-                        <div className="basket-items-wrp">
-                            <img src={Raspberry} alt="img" />
-                            <div className='basket-item'>
-                                <h4 className='basket-item-title'>
-                                    Fresh Raspberry
-                                </h4>
-                                <p className='basket-item-txt'>$30.00</p>
-                            </div>
-                        </div>
-                    </section>
-                    <div className='basket-btn-wrp'>
-                        <button className='basket-btn'>
-                            <span className='basket-btn-txt'>GO to checkout</span>
-                        </button>
-                    </div>
-                    <div className='basket-empty-container'>
-                        <div className='basket-empty-wrp'>
-                            <img src={BasketPng} alt="basket-img" />
-                            <h4 className='bskt-title'>IS EMPTY</h4>
-                            <p className='bskt-txt'>Add something to order</p>
-                            <button className='bskt-empty-btn'>
-                                <a href=""></a>
-                                SHOP NOW
-                            </button>
-                        </div>
-                    </div>
                     {cartItems.length > 0 ? (
-                      <div>
-                          <section className='basket-products'>
-                              {cartItems.map((obj, index) => (
-                                <div key={index} className="basket-items-wrp">
-                                    <img width={110} height={110} src={obj.imageUrl} alt={obj.title}/>
-                                    <div className='basket-item'>
-                                        <h4 className='basket-item-title'>
-                                            {obj.title}
-                                        </h4>
-                                        <p className='basket-item-txt'>${obj.price}</p>
+                        <div className='bskt-products-wrp'>
+                            <section className='basket-products'>
+                                {cartItems.map((obj, index) => (
+                                    <div key={index} className="basket-items-wrp">
+                                        <img width={110} height={110} src={obj.imageUrl} alt={obj.title} />
+                                        <div className='basket-item'>
+                                            <h4 className='basket-item-title'>
+                                                {obj.title}
+                                            </h4>
+                                            <p className='basket-item-txt'>${obj.price}</p>
+                                        </div>
                                     </div>
-                                </div>
-                              ))}
-                          </section>
-                          <div className='basket-btn-wrp'>
-                              <button className='basket-btn'>
-                                  <span className='basket-btn-txt'>GO to checkout</span>
-                              </button>
-                          </div>
-                      </div>
+                                ))}
+                            </section>
+                            <div className='basket-btn-wrp'>
+                                <button className='basket-btn'>
+                                    <span className='basket-btn-txt'>GO to checkout</span>
+                                </button>
+                            </div>
+                        </div>
                     ) : (
-                      <div className='basket-empty-container'>
-                          <div className='basket-empty-wrp'>
-                              <img src={BasketPng} alt="basket-img"/>
-                              <h4 className='bskt-title'>IS EMPTY</h4>
-                              <p className='bskt-txt'>Add something to order</p>
-                              <button className='bskt-empty-btn'>
-                                  SHOP NOW
-                              </button>
-                          </div>
-                      </div>
+                        <div className='basket-empty-container'>
+                            <div className='basket-empty-wrp'>
+                                <img src={BasketPng} alt="basket-img" />
+                                <h4 className='bskt-title'>IS EMPTY</h4>
+                                <p className='bskt-txt'>Add something to order</p>
+                                <Link to="/">
+                                    <button className='bskt-empty-btn'>
+                                        SHOP NOW
+                                    </button>
+                                </Link>
+                            </div>
+                        </div>
                     )}
 
                 </div>
-            </main>
+            </div>
         </>
     );
 };
