b5de542fd6ce365228960f58d843209f39031840
ychkydyk <zavdv1@gmail.com>
2023-02-19 02:47:56 +0300

    Заказ не уходит, ищу решение

--- client/src/components/Basket/Basket.jsx
+++ client/src/components/Basket/Basket.jsx
@@ -5,10 +5,40 @@ import './basket-style.scss';
 import Raspberry from './img/raspberry.png'
 import BasketPng from './img/basket.png'
 import useFetch from "../../hooks/useFetch";
+import {useSelector} from "react-redux";
+import {makeRequest} from "../../makeRequest";
+import { loadStripe } from "@stripe/stripe-js";
+
 
 const Basket = () => {
     const { cartItems } = useContext(AuthContext)
 
+    const totalPrice = () => {
+        let total = 0;
+        cartItems.forEach((item) => {
+            total += cartItems.slice(-1).length * item.attributes.price;
+        });
+        return total.toFixed(2);
+    };
+    const stripePromise = loadStripe(
+        "pk_test_51MS8CGDhtufCoDjnZyf7MYjgOOjpS7OPMLd0RRfnO5xTJjNotjTNT4xB5N9V72Znd5CnXxrThvAHQVtwdIAyHuOF00Mh08hlMX"
+    );
+    const handlePayment = async () => {
+        try {
+            const stripe = await stripePromise;
+            const res = await makeRequest.post("/orders", {
+                cartItems,
+            });
+            await stripe.redirectToCheckout({
+                sessionId: res.data.stripeSession.id,
+            });
+
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+console.log(cartItems)
     const { data } = useFetch(
       `/products?populate=*`
     );
@@ -21,7 +51,7 @@ const Basket = () => {
                             My Cart
                         </h3>
                         <div className='head-basket-txt'>
-                            <p className='head-basket-txt_2'>Total Price: $<span>333.00</span></p>
+                            <p className='head-basket-txt_2'>Total Price: $<span>{totalPrice()}</span></p>
                         </div>
                     </header>
                     {cartItems.length > 0 ? (
@@ -40,7 +70,7 @@ const Basket = () => {
                                 ))}
                             </section>
                             <div className='basket-btn-wrp'>
-                                <button className='basket-btn'>
+                                <button className='basket-btn'  onClick={handlePayment} >
                                     <span className='basket-btn-txt'>GO to checkout</span>
                                 </button>
                             </div>
--- client/src/redux/cartReducer.js
+++ client/src/redux/cartReducer.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  products: [],
+  cartItems: [],
 };
 
 export const cartSlice = createSlice({
@@ -9,18 +9,18 @@ export const cartSlice = createSlice({
   initialState,
   reducers: {
     addToCart: (state, action) => {
-      const item = state.products.find((item) => item.id === action.payload.id);
+      const item = state.cartItems.find((item) => item.id === action.payload.id);
       if (item) {
         item.quantity += action.payload.quantity;
       } else {
-        state.products.push(action.payload);
+        state.cartItems.push(action.payload);
       }
     },
     removeItem: (state,action) => {
-      state.products=state.products.filter(item=>item.id !== action.payload)
+      state.cartItems=state.cartItems.filter(item=>item.id !== action.payload)
     },
     resetCart: (state) => {
-      state.products = []
+      state.cartItems = []
     },
   },
 });
