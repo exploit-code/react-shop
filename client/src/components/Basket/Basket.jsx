43a6a6d2c5d26308212e5b363b1f611123ee18fd
Max1k86 <hasanovmaxim@yandex.ru>
2023-02-19 16:11:45 +0500

    изменения в вёрстке, корзина готова.

--- client/src/components/Basket/Basket.jsx
+++ client/src/components/Basket/Basket.jsx
@@ -4,9 +4,14 @@ import { AuthContext } from "../../context/UserContext";
 import './basket-style.scss';
 import Raspberry from './img/raspberry.png'
 import BasketPng from './img/basket.png'
+import useFetch from "../../hooks/useFetch";
 
 const Basket = () => {
     const { cartItems } = useContext(AuthContext)
+
+    const { data } = useFetch(
+        `/products?populate=*`
+    );
     return (
         <>
             <div className="page-basket-wrp">
@@ -22,14 +27,27 @@ const Basket = () => {
                     {cartItems.length > 0 ? (
                         <div className='bskt-products-wrp'>
                             <section className='basket-products'>
-                                {cartItems.map((obj, index) => (
-                                    <div key={index} className="basket-items-wrp">
-                                        <img width={110} height={110} src={obj.imageUrl} alt={obj.title} />
-                                        <div className='basket-item'>
-                                            <h4 className='basket-item-title'>
-                                                {obj.title}
-                                            </h4>
-                                            <p className='basket-item-txt'>${obj.price}</p>
+                                {cartItems.map((item, index) => (
+                                    <div className='basket-product-total'>
+                                        <div key={index} className="basket-items-wrp">
+                                            <img width={110} height={110} src={process.env.REACT_APP_UPLOAD_URL + item.attributes?.img?.data?.attributes?.url} alt={item.attributes.title} />
+                                            <div className='basket-item'>
+                                                <h4 className='basket-item-title'>
+                                                    {item.attributes.title}
+                                                </h4>
+                                                <p className='basket-item-txt'>${item.attributes.price}</p>
+                                            </div>
+                                        </div>
+                                        <div className='basket-total'>
+                                            <div className='basket-total-info'>
+                                                <h3 className='basket-total-title'>$30</h3>
+                                                <button className='basket-total-count-btn'>-</button>
+                                                <span className='basket-total-txt'>Kol-vo</span>
+                                                <button className='basket-total-count-btn'>+</button>
+                                            </div>
+                                            <div>
+                                                <button className='basket-total-remove-btn'>Remove</button>
+                                            </div>
                                         </div>
                                     </div>
                                 ))}
