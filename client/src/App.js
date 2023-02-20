a4f065f72e29b815f2c41bed8ed506bc8e5503ab
Lyubov <zenkova.lyuba@gmail.com>
2023-02-20 15:23:35 +0300

    добавлены разделы AboutUs и AboutOurShop

--- client/src/App.js
+++ client/src/App.js
@@ -7,58 +7,67 @@ import Register from './components/Register/Register';
 import PrivateRoute from './routes/PrivateRoute';
 import Basket from './components/Basket/Basket';
 import ProductsPage from "./pages/ProductsPage/ProductsPage";
-
-import Catalog from "./components/Catalog/Catalog";
-import ProductPage from "./pages/ProductPage/ProductPage";
+import AboutAs from './components/AboutUs/AboutUs'
+import AboutOurShop from './components/AboutOurShop/AboutOurShop'
+import Catalog from './components/Catalog/Catalog'
+import ProductPage from './pages/ProductPage/ProductPage'
 
 function App() {
-    const router = createBrowserRouter([
-      {
-        path: '/',
-        element: <Layout />,
-        children: [
-          {
-            path: '/',
-            element: <HomePage />,
-          },
-          {
-            path: '/basket',
-            element: <Basket />,
-          },
-          {
-            path: 'login',
-            element: <Login />,
-          },
-          {
-            path: 'register',
-            element: <Register />,
-          },
-          {
-            path: 'likes',
-            element: <></>,
-          },
-          {
-            path: '/products/:id',
-            element: <ProductsPage />,
-          },
-          {
-            path: '/productpage/:id',
-            element: <ProductPage/>,
-          },
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        {
+          path: '/',
+          element: <HomePage />,
+        },
+        {
+          path: '/basket',
+          element: <Basket />,
+        },
+        {
+          path: 'login',
+          element: <Login />,
+        },
+        {
+          path: 'register',
+          element: <Register />,
+        },
+        {
+          path: 'likes',
+          element: <></>,
+        },
+        {
+          path: '/products/:id',
+          element: <ProductsPage />,
+        },
+        {
+          path: '/productpage/:id',
+          element: <ProductPage />,
+        },
 
-          {
-            path: '/catalog',
-            element: <Catalog />,
-          },
-        ],
-      },
-    ])
+        {
+          path: '/catalog',
+          element: <Catalog />,
+        },
+        {
+          path: '/aboutus',
+          element: <AboutAs />,
+        },
+        {
+          path: '/aboutourshop',
+          element: <AboutOurShop />,
+        },
+      ],
+    },
+  ])
 
-    return (
-        <div className="app">
-            <RouterProvider router={router}></RouterProvider>
-        </div>
-    );
+  return (
+    <div className='app'>
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  )
 }
 
 export default App;
