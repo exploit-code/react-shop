179d6a843249c0a824464a97cdb19b971eaeca70
aborushko <o_xx@list.ru>
2023-02-20 22:47:57 +0300

    fixed login

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -5,25 +5,22 @@ import { Link } from 'react-router-dom';
 import './Login.scss'
 
 const Login = () => {
-    const { signIn } = useContext(AuthContext);
-    const navigate = useNavigate
+    const { signIn, user } = useContext(AuthContext);
+    const navigate = useNavigate();
 
     const handleSubmit = event => {
         event.preventDefault();
         const form = event.target;
         const email = form.email.value;
         const password = form.password.value;
-        // console.log(email, password);
 
         signIn(email, password)
             .then(result => {
-                const user = result.user;
-                // console.log(user);
                 form.reset();
                 navigate('/');
             })
             .catch(error => {
-                console.error(error);
+                // console.error(error);
             })
     }
     return (
