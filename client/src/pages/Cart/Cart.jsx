0ba8f9069d9885a939513714bf113a33b480ddf9
Max1k86 <hasanovmaxim@yandex.ru>
2023-03-03 22:32:54 +0500

    Изменение способа оплаты в форме корзины.
    добавил состояние для радио кнопок и написал функции для их изменения. Так же функция которая запускает нужный способ оплаты при нажатии на кнопку "Place on order".

--- client/src/pages/Cart/Cart.jsx
+++ client/src/pages/Cart/Cart.jsx
@@ -18,7 +18,7 @@ const Cart = () => {
 
   //Checkout
   const stripePromise = loadStripe(
-      "pk_test_51MS8CGDhtufCoDjnZyf7MYjgOOjpS7OPMLd0RRfnO5xTJjNotjTNT4xB5N9V72Znd5CnXxrThvAHQVtwdIAyHuOF00Mh08hlMX"
+    "pk_test_51MS8CGDhtufCoDjnZyf7MYjgOOjpS7OPMLd0RRfnO5xTJjNotjTNT4xB5N9V72Znd5CnXxrThvAHQVtwdIAyHuOF00Mh08hlMX"
   );
 
 
@@ -49,80 +49,78 @@ const Cart = () => {
   }
 
   return (
-      <>
-        <div className="page-cart-wrp">
-          <div className="cart">
-            <header className='head-cart-wrp'>
-              <h3 className='head-cart-title'>
-                My Cart
-              </h3>
-            </header>
-            {cartItems.length > 0 ? (
-                <div className='cart-products-wrp'>
-                  <section className='cart-products'>
-                    {cartItems.map((item, idx) => (
-                        <div key={idx} className='cart-product-total'>
-                          <Link to={`/productpage/${item.id}`} className="cart-items-wrp">
-                            <div className='overlay'>
-                              <img className='cart-item-img' src={process.env.REACT_APP_UPLOAD_URL + item.img}
-                                   alt={item.title} />
-                            </div>
-                            <div className='cart-item'>
-                              <h4 className='cart-item-title'>
-                                {item.title}
-                              </h4>
-                              <p className='cart-item-txt'>${item.price}</p>
-                            </div>
-                          </Link>
-                          <div className='cart-total'>
-                            <div className='cart-total-info'>
-                              <h3 className='cart-total-title'>${item.totalPriceItem}</h3>
-                              <button onClick={() => dispatch(deleteItem(item))} className='cart-total-count-btn'>-</button>
-                              <span className='cart-total-txt'>{item.quantity}</span>
-                              <button onClick={() => dispatch(addItem(item))} className='cart-total-count-btn'>+</button>
-                            </div>
-                            <div>
-                              <button className='cart-total-remove-btn'
-                                      onClick={() => dispatch(removeItems(item))}
-                              >
-                                Remove
-                              </button>
-                            </div>
-                          </div>
-                        </div>
-                    ))}
-                  </section>
-
-                  <div className='cart-btn-wrp'>
-                    <button onClick={checkoutPayment} type='submit' className='cart-btn' >Pay by Card
-                    </button>
-                    <button className='cart-btn' onClick={() => clickShopOrder()}>
-                  <span className='cart-btn-txt'>
-                    Cash/ uppon receipt
-                  </span>
-                    </button>
-                  </div>
-
-                  {shopOrder && <Form className={className} />}
-                </div>
-            ) : (
-                <div className='cart-empty-container'>
-                  <div className='cart-empty-wrp'>
-                    <img src={cartPng} alt="cart-img" />
-                    <h4 className='cart-title'>IS EMPTY</h4>
-                    <p className='cart-txt'>Add something to order</p>
-                    <Link to="/">
-                      <button className='cart-btn'>
-                        SHOP NOW
-                      </button>
+    <>
+      <div className="page-cart-wrp">
+        <div className="cart">
+          <header className='head-cart-wrp'>
+            <h3 className='head-cart-title'>
+              My Cart
+            </h3>
+          </header>
+          {cartItems.length > 0 ? (
+            <div className='cart-products-wrp'>
+              <section className='cart-products'>
+                {cartItems.map((item, idx) => (
+                  <div key={idx} className='cart-product-total'>
+                    <Link to={`/productpage/${item.id}`} className="cart-items-wrp">
+                      <div className='overlay'>
+                        <img className='cart-item-img' src={process.env.REACT_APP_UPLOAD_URL + item.img}
+                          alt={item.title} />
+                      </div>
+                      <div className='cart-item'>
+                        <h4 className='cart-item-title'>
+                          {item.title}
+                        </h4>
+                        <p className='cart-item-txt'>${item.price}</p>
+                      </div>
                     </Link>
+                    <div className='cart-total'>
+                      <div className='cart-total-info'>
+                        <h3 className='cart-total-title'>${item.totalPriceItem}</h3>
+                        <button onClick={() => dispatch(deleteItem(item))} className='cart-total-count-btn'>-</button>
+                        <span className='cart-total-txt'>{item.quantity}</span>
+                        <button onClick={() => dispatch(addItem(item))} className='cart-total-count-btn'>+</button>
+                      </div>
+                      <div>
+                        <button className='cart-total-remove-btn'
+                          onClick={() => dispatch(removeItems(item))}
+                        >
+                          Remove
+                        </button>
+                      </div>
+                    </div>
                   </div>
-                </div>
-            )}
+                ))}
+              </section>
 
-          </div>
+              <div className='cart-btn-wrp'>
+                <button className='cart-btn' onClick={() => clickShopOrder()}>
+                  <span className='cart-btn-txt'>
+                    Go to checkout
+                  </span>
+                </button>
+              </div>
+
+              {shopOrder && <Form className={className} />}
+            </div>
+          ) : (
+            <div className='cart-empty-container'>
+              <div className='cart-empty-wrp'>
+                <img src={cartPng} alt="cart-img" />
+                <h4 className='cart-title'>IS EMPTY</h4>
+                <p className='cart-txt'>Add something to order</p>
+                <Link to="/">
+                  <button className='cart-btn'>
+                    SHOP NOW
+                  </button>
+                </Link>
+              </div>
+            </div>
+          )}
+
         </div>
-      </>
+      </div>
+    </>
   );
 };
 
