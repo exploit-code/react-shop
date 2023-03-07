59955341298d3ac33b486f0927f8c58b739bde0e
kripota <dyupin.lesha@yandex.ru>
2023-03-07 22:41:33 +0700

    Скорректировал hover у кнопок, поправил отображение элементов на странице товараэ

--- client/src/components/FilteredColumn/FilteredColumn.jsx
+++ client/src/components/FilteredColumn/FilteredColumn.jsx
@@ -5,12 +5,12 @@ import arrowRight from './img/arrow_right.png';
 import arrowLeft from './img/arrow_left.png';
 
 import FilteredColumnList from "../FilteredColumnList/FilteredColumnList";
-import {useState} from "react";
+import { useState } from "react";
 
 const FilteredColumn = ({ type }) => {
 
-const [firstIdx, setFirstIdx] = useState(0);
-const secondIdx = firstIdx+3;
+  const [firstIdx, setFirstIdx] = useState(0);
+  const secondIdx = firstIdx + 3;
 
   const arrRight = () => {
     setFirstIdx(firstIdx + 3);
@@ -21,16 +21,17 @@ const secondIdx = firstIdx+3;
 
   return (
     <>
-        <div className="filterBlock">
-          <div className="filterBlock__latestProducts">
-            <div className="filterBlock__latestProducts_titleBlock">
-              <div className="filterBlock__latestProducts_titleBlock_title">{type} products</div>
-              <img  onClick={arrLeft} className="filterBlock__latestProducts_titleBlock_arrowLeft" src={arrowLeft} alt="arrow left"></img>
-             <img onClick={arrRight} className="filterBlock__latestProducts_titleBlock_arrowRight" id="show-more"  src={arrowRight} alt="arrow right"></img>
-            </div>
-            <FilteredColumnList firstIdx={firstIdx} secondIdx={secondIdx} type={type} />
+      <div className="filterBlock">
+        <div className="filterBlock__latestProducts">
+          <div className="filterBlock__latestProducts_titleBlock">
+            <div className="filterBlock__latestProducts_titleBlock_title">{type} products</div>
+            {/* <img  onClick={arrLeft} className="filterBlock__latestProducts_titleBlock_arrowLeft" src={arrowLeft} alt="arrow left"></img> */}
+            <div onClick={arrLeft} className="filterBlock__latestProducts_titleBlock_arrowLeft">&#8249;</div>
+            <div onClick={arrLeft} className="filterBlock__latestProducts_titleBlock_arrowRight">&#8250;</div>
           </div>
+          <FilteredColumnList firstIdx={firstIdx} secondIdx={secondIdx} type={type} />
         </div>
+      </div>
     </>
   )
 }
