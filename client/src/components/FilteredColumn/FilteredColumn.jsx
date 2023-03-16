2b84d3b1153fda3d1a5091123d5c40aa09294841
ychkydyk <zavdv1@gmail.com>
2023-03-16 03:00:36 +0300

    проверка на конец массива + перенес пропсы и стили в дочерний компонент

--- client/src/components/FilteredColumn/FilteredColumn.jsx
+++ client/src/components/FilteredColumn/FilteredColumn.jsx
@@ -1,35 +1,20 @@
 import '../../normalize.css';
 import './FilteredColumn.scss';
 
-import arrowRight from './img/arrow_right.png';
-import arrowLeft from './img/arrow_left.png';
-
 import FilteredColumnList from "../FilteredColumnList/FilteredColumnList";
-import { useState } from "react";
+
 
 const FilteredColumn = ({ type }) => {
-
-  const [firstIdx, setFirstIdx] = useState(0);
-  const secondIdx = firstIdx + 3;
-
-  const arrRight = () => {
-    setFirstIdx(firstIdx + 3);
-  }
-  const arrLeft = () => {
-    setFirstIdx(firstIdx - 3);
-  }
-
   return (
     <>
       <div className="filterBlock">
         <div className="filterBlock__latestProducts">
           <div className="filterBlock__latestProducts_titleBlock">
-            <div className="filterBlock__latestProducts_titleBlock_title">{type} products</div>
-            {/* <img  onClick={arrLeft} className="filterBlock__latestProducts_titleBlock_arrowLeft" src={arrowLeft} alt="arrow left"></img> */}
-            <div onClick={arrLeft} className="filterBlock__latestProducts_titleBlock_arrowLeft">&#8249;</div>
-            <div onClick={arrRight} className="filterBlock__latestProducts_titleBlock_arrowRight">&#8250;</div>
+            <div className="filterBlock__latestProducts_titleBlock_title">
+              {type} products
+            </div>
           </div>
-          <FilteredColumnList firstIdx={firstIdx} secondIdx={secondIdx} type={type} />
+          <FilteredColumnList   type={type} />
         </div>
       </div>
     </>
