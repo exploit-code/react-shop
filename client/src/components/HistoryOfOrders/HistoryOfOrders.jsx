6cd2aa45166e67b23bd5b385dda9af97650fdcc7
Sergey Lopatin <aid.94@mail.ru>
2023-04-04 12:30:37 +0000

    fix-profileMobileLayout+addAnchors

--- client/src/components/HistoryOfOrders/HistoryOfOrders.jsx
+++ client/src/components/HistoryOfOrders/HistoryOfOrders.jsx
@@ -10,6 +10,7 @@ import CircularProgress from "@mui/material/CircularProgress"
 import { Link } from 'react-router-dom'
 
 const HistoryOfOrders = () => {
+
   const { user } = useContext(AuthContext)
   const { data, error, loading } = useFetch(
     `orders?filters[mail][$eq]=${user.email}`
@@ -38,95 +39,105 @@ const HistoryOfOrders = () => {
     setValue(e.target.value)
   }
 
+  const avatarName = user.displayName?.slice(0, 2);
+
   return (
-    <div className='historyOfOrders'>
-      <div className='historyOfOrders_leftPart'>
-        <LeftSideProfile />
-      </div>
-      <form className='historyOfOrders_rightPart'>
-        <div className='historyOfOrders_heading'>History of orders</div>
-        <div className='historyOfOrders_filter_box'>
-          <ul className='historyOfOrders_filter_ul'>
-            <input
-              className='historyOfOrders_filter_input'
-              id='1'
-              type='radio'
-              name='radio'
-              value='all'
-              onChange={handleClick}
-              checked={value === 'all'}
-            />
-            <li className='historyOfOrders_filter_link linkorders_first'>
-              <label className='historyOfOrders_filter_label' htmlFor='1'>
-                All orders ({data?.length})
-              </label>
-            </li>
-            <input
-              className='historyOfOrders_filter_input'
-              id='2'
-              type='radio'
-              name='radio'
-              value='current'
-              onChange={handleClick}
-              checked={value === 'current'}
-            />
-            <li className='historyOfOrders_filter_link'>
-              <label className='historyOfOrders_filter_label' htmlFor='2'>
-                Current ({sumCurrentOrders?.length})
-              </label>
-            </li>
-            <input
-              className='historyOfOrders_filter_input'
-              id='3'
-              type='radio'
-              name='radio'
-              value='completed'
-              onChange={handleClick}
-              checked={value === 'completed'}
-            />
-            <li className='historyOfOrders_filter_link'>
-              <label className='historyOfOrders_filter_label' htmlFor='3'>
-                Completed ({sumCompletedOrders?.length})
-              </label>
-            </li>
-            <input
-              className='historyOfOrders_filter_input'
-              id='4'
-              type='radio'
-              name='radio'
-              value='canceled'
-              onChange={handleClick}
-              checked={value === 'canceled'}
-            />
-            <li className='historyOfOrders_filter_link'>
-              <label className='historyOfOrders_filter_label' htmlFor='4'>
-                Canceled ({sumCanceledOrders?.length})
-              </label>
-            </li>
-          </ul>
-          <div className='historyOfOrders_heading-mini_box'>
-            <div className='historyOfOrders_heading-mini'>{value}</div>
-            <div className='historyOfOrders_heading-mini'>
-              {selectOrders?.length} orders
+    // <div className='historyOfOrders'>
+    <>
+      <div className='grid'>
+        <div className='grid__box'>
+          <LeftSideProfile avatarName={avatarName} />
+        </div>
+        <div className='grid__box'>
+          <form id='historyOfOrdersInfo' className='historyOfOrders_rightPart'>
+            <div className='historyOfOrders_heading'>History of orders</div>
+            <div className='historyOfOrders_filter_box'>
+              <ul className='historyOfOrders_filter_ul'>
+                <input
+                  className='historyOfOrders_filter_input'
+                  id='1'
+                  type='radio'
+                  name='radio'
+                  value='all'
+                  onChange={handleClick}
+                  checked={value === 'all'}
+                />
+                <li className='historyOfOrders_filter_link linkorders_first'>
+                  <label className='historyOfOrders_filter_label' htmlFor='1'>
+                    All orders ({data?.length})
+                  </label>
+                </li>
+                <input
+                  className='historyOfOrders_filter_input'
+                  id='2'
+                  type='radio'
+                  name='radio'
+                  value='current'
+                  onChange={handleClick}
+                  checked={value === 'current'}
+                />
+                <li className='historyOfOrders_filter_link'>
+                  <label className='historyOfOrders_filter_label' htmlFor='2'>
+                    Current ({sumCurrentOrders?.length})
+                  </label>
+                </li>
+                <input
+                  className='historyOfOrders_filter_input'
+                  id='3'
+                  type='radio'
+                  name='radio'
+                  value='completed'
+                  onChange={handleClick}
+                  checked={value === 'completed'}
+                />
+                <li className='historyOfOrders_filter_link'>
+                  <label className='historyOfOrders_filter_label' htmlFor='3'>
+                    Completed ({sumCompletedOrders?.length})
+                  </label>
+                </li>
+                <input
+                  className='historyOfOrders_filter_input'
+                  id='4'
+                  type='radio'
+                  name='radio'
+                  value='canceled'
+                  onChange={handleClick}
+                  checked={value === 'canceled'}
+                />
+                <li className='historyOfOrders_filter_link'>
+                  <label className='historyOfOrders_filter_label' htmlFor='4'>
+                    Canceled ({sumCanceledOrders?.length})
+                  </label>
+                </li>
+              </ul>
+              <div className='historyOfOrders_heading-mini_box'>
+                <div className='historyOfOrders_heading-mini'>{value}</div>
+                <div className='historyOfOrders_heading-mini'>
+                  {selectOrders?.length} orders
+                </div>
+              </div>
+              <div className='historyOfOrders_line'></div>
             </div>
-          </div>
-          <div className='historyOfOrders_line'></div>
+            <div className='historyOfOrders_orders_box'>
+              {error ? (
+                <Alert severity='error'>Something went wrong!</Alert>
+              ) : loading ? (
+                <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+                  <CircularProgress color='success' />
+                </Box>
+              ) : (
+                selectOrders?.map((order, i) => {
+                  return <HistoryOfOrdersCard value={value} order={order} key={i} />
+                })
+              )}
+            </div>
+          </form>
         </div>
-        <div className='historyOfOrders_orders_box'>
-          {error ? (
-            <Alert severity='error'>Something went wrong!</Alert>
-          ) : loading ? (
-            <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-              <CircularProgress color='success' />
-            </Box>
-          ) : (
-            selectOrders?.map((order, i) => {
-              return <HistoryOfOrdersCard value={value} order={order} key={i} />
-            })
-          )}
-        </div>
-      </form>
-    </div>
+      </div>
+      {/* <div className='historyOfOrders_leftPart'>
+        <LeftSideProfile avatarName={avatarName} />
+      </div> */}
+    </>
   )
 }
 
