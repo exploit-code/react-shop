b32030cde64c99d2bd19e9b9e08bd78a084bceef
ychkydyk <zavdv1@gmail.com>
2023-04-06 21:54:49 +0300

    we did it!

--- client/src/components/BannerMainPage/BannerMainPage.js
+++ client/src/components/BannerMainPage/BannerMainPage.js
@@ -7,9 +7,9 @@ const BannerMainPage = () => {
             <div className='bannermainpage__top'>
                 <div className='bannermainpage__top_poster'>
                     <div className='bannermainpage__top_poster_left'>
-                        <p className='bannermainpage__top_poster_left_p_ff'>FRUIT FRESH</p>
-                        <p className='bannermainpage__top_poster_left_h1'>Vegetable <br></br>100% Organic</p>
-                        <p className='bannermainpage__top_poster_left_p_free'>Free Pickup and Delivery Available</p>
+                        <p className='bannermainpage__top_poster_left_p_ff'>&#128515; &#128515; &#128515;</p>
+                        <p className='bannermainpage__top_poster_left_h1'>Yeahhhhh!  <br></br>Good-Team!</p>
+                        <p className='bannermainpage__top_poster_left_p_free'>Vsem spasibo!  We did it!!!!👏👏👏</p>
                     </div>
                     <div className='bannermainpage__top_poster_right'>
                         <img className='bannermainpage__top_poster_right_img' src={poster} alt='poster'></img>
